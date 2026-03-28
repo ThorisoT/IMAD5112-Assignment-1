85e8b8155831bb8ce5f3cbbb90e5e37b48c9a63c
ThorisoT <st10191543@myemeris.edu.za>
2026-03-28 11:27:29 +0200

    Added UI components, Submit Button  and  Social messages.

--- app/src/main/java/com/example/imad5112_assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/imad5112_assignment_1/MainActivity.kt
@@ -6,3 +6,28 @@ import android.widget.TextView
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val timeEditText = findViewById<EditText>(R.id.timeEditText)
+        val submitButton = findViewById<Button>(R.id.sumbitButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+
+        submitButton.setOnClickListener {
+
+            val timeInput = timeEditText.text.toString()
+
+            val message = when (timeInput) {
+
+                "Morning" -> "Send a 'Good morning' text to a family member."
+                "Mid-Morning" -> "Reach out to a colleague with a quick 'Thank you.'"
+                "Afternoon" -> "Share a funny meme or interesting link with a friend."
+                "Afternoon Snack Time" -> "Send a quick 'thinking of you' message."
+                "Dinner" -> "Call a friend or relative for a 5-minute catch-up."
+                "Night" -> "Leave a thoughtful comment on a friend's post."
+                else -> "Invalid input. Please enter: Morning, Mid-Morning, Afternoon, Afternoon Snack Time, Dinner or Night"
+            }
