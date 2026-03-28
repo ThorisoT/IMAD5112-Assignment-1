20a2e27714b196852cc8ac24ac12dd5aebf5fa3b
ThorisoT <st10191543@myemeris.edu.za>
2026-03-28 10:39:37 +0200

    Added Imports

---
+++ app/src/main/java/com/example/imad5112_assignment_1/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.imad5112_assignment_1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
